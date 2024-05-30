d5010beef2fc7782a89b62114d94992388c102aa
Jose Vargas <rafael.uruguay2022@gmail.com>
2024-05-30 15:24:10 -0300

    first commit

---
+++ js/carrito.js
@@ -0,0 +1,57 @@
+let cartStorage = localStorage.getItem("cartProducts")
+cartStorage = JSON.parse(cartStorage)
+
+let cartContainer = document.getElementById("cart-section")
+let totalContainer = document.getElementById("total-section")
+let buttonContainer = document.getElementById("button-section")
+
+function renderCarrito (carItems) {
+
+    let total = 0;
+
+    carItems.forEach (producto => {
+        const cart = document.createElement("div")
+        cart.innerHTML =` <h3> ${producto.nombre}</h3>
+                          <p>${producto.precio}</p>`
+        cartContainer.appendChild(cart)
+        
+        total += producto.precio
+    })
+
+    const totalElement = document.createElement("section");
+            totalElement.innerHTML = `<h3>Total: $${total.toFixed(2)}</h3>`;
+            totalContainer.appendChild(totalElement)
+
+    const buyButton = document.createElement("button");
+    buyButton.textContent = "Comprar";
+    buyButton.addEventListener("click", () => {
+
+        Toastify({
+            text: "Compra realizada, Gracias por visitarnos. Te esperamos nuevamente",
+            duration: 5000,
+            destination: "#",
+            newWindow: true,
+            close: true,
+            gravity: "top", // `top` or `bottom`
+            position: "left", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+              background: "linear-gradient(to right, #fe8801, #96c93d)",
+            },
+            onClick: function(){} // Callback after click
+          }).showToast();
+
+    localStorage.removeItem("cartProducts") // Vaciar el carrito
+    cartContainer.innerHTML = ""
+    totalContainer.innerHTML = ""
+    buttonContainer.innerHTML = ""           
+})
+        buttonContainer.appendChild(buyButton)
+
+
+}
+
+
+if (cartStorage) {
+    renderCarrito(cartStorage);
+}
---
+++ js/main.js
@@ -0,0 +1,87 @@
+const productos= [
+    {
+        id: 1,
+        nombre: "Banana Ecuador",
+        precio: 89
+
+    },
+    {
+        id: 2,
+        nombre: "Cebolla Blanca",
+        precio: 89
+    },
+    {
+        id: 3,
+        nombre: "Lechuga Comun",
+        precio: 109
+    },
+    {
+        id: 4,
+        nombre: "Papa Blanca",
+        precio: 89
+    },
+    {
+        id: 5,
+        nombre: "Naranja",
+        precio: 100
+    }
+]
+
+
+let CartProducts = []
+let CartPrdoctsLS =localStorage.getItem("cartProducts")
+if (CartPrdoctsLS) {
+    CartProducts =JSON.parse(CartPrdoctsLS)
+} else {
+    CartProducts = []
+}
+
+let productsContainer = document.getElementById("products-container")
+
+fetch("./db/data.json")
+.then(respuesta=>respuesta.json())
+.then(data=> {
+    data.forEach ( product => {
+        const card = document.createElement("div")
+        card.innerHTML =` <h3 class="claseCreateE"> ${product.nombre}</h3>
+                          <img class="img-div" src= ${product.img}  alt= ${product.img}>
+                          <p class="claseCreateE">$${product.precio}</p>
+                          <button class="productoAgregar" id="${product.id}">Agregar</button>`
+
+         productsContainer.appendChild(card)                 
+    })
+
+    addToCartButton()
+})
+
+.catch((error) => console.log(error))
+
+function addToCartButton () {
+    let addButton = document.querySelectorAll(".productoAgregar")
+    addButton.forEach(button => {
+        button.onclick = (e) => {
+            const productId = e.currentTarget.id
+            const selectedProduct = productos.find(product => product.id == productId)
+            CartProducts.push(selectedProduct)
+            console.log(CartProducts)
+
+            localStorage.setItem("cartProducts", JSON.stringify(CartProducts))
+
+            Toastify({
+                text: "Fruta agregada",
+                duration: 1000,
+                destination: "#",
+                newWindow: true,
+                gravity: "top", // `top` or `bottom`
+                position: "left", // `left`, `center` or `right`
+                stopOnFocus: true, // Prevents dismissing of toast on hover
+                style: {
+                  background: "linear-gradient(to right, #fe8801, #96c93d)",
+                },
+                onClick: function(){} // Callback after click
+              }).showToast();
+        }
+    })
+}
+
+
